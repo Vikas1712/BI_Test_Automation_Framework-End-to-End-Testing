90c10250631c3a66a9c7026617ff2e23b3edb822
Vikas Yadav <dnsvikas.wins@gmail.com>
2022-07-22 18:51:52 +0200

    Update SampleBash.sh

--- BatchExecution_BI/SampleBash.sh
+++ BatchExecution_BI/SampleBash.sh
@@ -12,9 +12,9 @@
  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/pwc/Informatica/10.2/server/bin
  echo Our current directory is $PWD
  echo Executing Workflow
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f KEY_CDM -wait wf_KEY_SELE_TICO001
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f KEY_CDM -wait wf_KEY_SELE_TICO002
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f KEY_CDM -wait wf_APPLY_WSLA_TICO001
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f EDW -wait wf_DIM_CALL_CONSULT_CONS001
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f EDW -wait wf_DIM_CALL_CENTER_WORKGROUP
- pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd TMNL -u ID029640 -p Password -f EDW -wait wf_FCT_WORKGROUP_CALL_CCC001
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f KEY_CDM -wait wf_KEY_SELE_TICO001
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f KEY_CDM -wait wf_KEY_SELE_TICO002
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f KEY_CDM -wait wf_APPLY_WSLA_TICO001
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f EDW -wait wf_DIM_CALL_CONSULT_CONS001
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f EDW -wait wf_DIM_CALL_CENTER_WORKGROUP
+ pmcmd startworkflow -sv PWC_INT_DEV -d PWC_Domain_DEV -usd DEMO -u ID -p Password -f EDW -wait wf_FCT_WORKGROUP_CALL_CCC001
